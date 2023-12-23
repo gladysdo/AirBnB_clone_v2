910875aea7f204a1b1f8c6669640734fda72a924
gladysdo <doe.glad@gmail.com>
2023-12-22 23:49:53 -0800

    Hello Flask!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,13 +16,13 @@ def do_deploy(archive_path):
         arc = archive_path.split("/")
         base = arc[1].strip('.tgz')
         put(archive_path, '/tmp/')
-        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        run('mkdir -p /data/web_static/releases/{}'.format(base))
         main = "/data/web_static/releases/{}".format(base)
-        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
-        sudo('rm /tmp/{}'.format(arc[1]))
-        sudo('mv {}/web_static/* {}/'.format(main, main))
-        sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
+        run('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        run('rm /tmp/{}'.format(arc[1]))
+        run('mv {}/web_static/* {}/'.format(main, main))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
     except:
         return False
