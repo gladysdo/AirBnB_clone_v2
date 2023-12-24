4018d8c5b968b67e8ac0cd9c838f62ee5f41cb5e
gladysdo <doe.glad@gmail.com>
2023-12-24 15:04:50 -0800

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def num_display(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_html(n):
     """display HTML is "n" is a number only"""
-    return render_template('5-number.html', name=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
