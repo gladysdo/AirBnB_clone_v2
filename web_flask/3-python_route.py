0cec54e409775b25df3cbdc8bedfc4a95e3e7159
gladysdo <doe.glad@gmail.com>
2023-12-24 15:51:37 -0800

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,27 +1,36 @@
+#!/usr/bin/python3
+"""Flask framework
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return 'HBNB'
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """return hello hbhb
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """return HBNB"""
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    # Replace underscores with spaces
-    text_with_spaces = text.replace('_', ' ')
-    return 'C {}'.format(text_with_spaces)
+def text(text):
+    """return text given"""
+    return "C {}".format(text.replace("_", " "))
 
-@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+
+@app.route('/python/', defaults={'text': 'is_cool'})
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    # Replace underscores with spaces
-    text_with_spaces = text.replace('_', ' ')
-    return 'Python {}'.format(text_with_spaces)
+def display(text):
+    """display “Python ”, followed by the value of the text"""
+    return "Python {}".format(text.replace("_", " "))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
