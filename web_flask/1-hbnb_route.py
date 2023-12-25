8c616c38de44d300fcf40ae0ad0e7744d1e50340
gladysdo <doe.glad@gmail.com>
2023-12-24 16:23:56 -0800

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,13 +1,19 @@
+#!/usr/bin/python3
+"""Flask framework
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """ return hello HBNB """
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """ return HBNB """
     return 'HBNB'
 
 if __name__ == '__main__':
